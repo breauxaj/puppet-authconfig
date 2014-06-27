a528638a6e2982124f78f90c6b5a9befa0f7675a
Adam Breaux <breauxaj@gmail.com>
2014-06-27 14:51:00 -0400

    skeleton

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
-class authconfig {
+class authconfig (
+  $ensure = 'latest'
+){
+  $required = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+
+    ]
+  }
+
+  package { $required: ensure => $ensure }
 
 }
